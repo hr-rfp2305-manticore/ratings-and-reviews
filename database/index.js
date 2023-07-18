6d04eed3eed2541061252e6abe296128c8da4238
Yichen <chenlin7789@gmail.com>
2023-07-18 09:57:51 -0700

    updated ETL process

--- database/index.js
+++ database/index.js
@@ -1,13 +1,15 @@
-const createReviewsPhotosCollection = require('./createReviewsPhotosCollection.js');
-const createProductReviews = require('./createProductReviews.js');
-const createCharacteristicReviewsCollection = require('./createCharacteristicReviewsCollection.js');
-const createReviewsMetadata = require('./createReviewsMetadata.js');
+const createReviewsPhotosCollection = require('./ETL/createReviewsPhotosCollection.js');
+const createProductReviews = require('./ETL/createProductReviews.js');
+const createCharacteristicReviewsCollection = require('./ETL/createCharacteristicReviewsCollection.js');
+const createReviewsMetadata = require('./ETL/createReviewsMetadata.js');
+const createCharacteristicCollection = require('./ETL/createCharacteristicCollection.js');
 
 const aggregateData = async () => {
   try {
     await createReviewsPhotosCollection();
     await createProductReviews();
     await createCharacteristicReviewsCollection();
+    await createCharacteristicCollection();
     await createReviewsMetadata()
   } catch (error) {
     console.log(error);
