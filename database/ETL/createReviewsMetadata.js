dfa7137727c6631e1208e232c0929a7c67dc1fb8
Yichen <chenlin7789@gmail.com>
2023-07-18 09:50:36 -0700

    updated ETL process

---
+++ database/ETL/createReviewsMetadata.js
@@ -0,0 +1,167 @@
+const { MongoClient } = require('mongodb');
+
+const createReviewsMetadata = async () => {
+  const client = await MongoClient.connect('mongodb://localhost:27017', {
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db('hr-sdc-manticore');
+
+  const reviews = db.collection('reviews');
+  // await reviews.createIndex({ id: 1 });
+
+  // start timer
+  console.time('create reviews_metadata');
+
+  const cursor = reviews.aggregate([
+    {
+      $group:
+        {
+          _id: "$product_id",
+          rating_0: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 0],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          rating_1: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 1],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          rating_2: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 2],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          rating_3: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 3],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          rating_4: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 4],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          rating_5: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$rating", 5],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          recommend_true: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$recommend", "true"],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+          recommend_false: {
+            $sum: {
+              $cond: [
+                {
+                  $eq: ["$recommend", "false"],
+                },
+                1,
+                0,
+              ],
+            },
+          },
+        },
+    },
+    {
+      $project:
+        {
+          product_id: "$_id",
+          ratings: {
+            0: "$rating_0",
+            1: "$rating_1",
+            2: "$rating_2",
+            3: "$rating_3",
+            4: "$rating_4",
+            5: "$rating_5",
+          },
+          recommended: {
+            1: "$recommend_true",
+            0: "$recommend_false",
+          },
+        },
+    },
+    {
+      $lookup:
+        {
+          from: "characteristics_collection",
+          localField: "product_id",
+          foreignField: "_id",
+          as: "result",
+        },
+    },
+    {
+      $addFields:
+        {
+          temp: {
+            $arrayElemAt: ["$result", 0],
+          },
+          result: "$$REMOVE",
+        },
+    },
+    {
+      $addFields:
+        {
+          characteristics: "$temp.characteristics",
+          temp: "$$REMOVE",
+        },
+    },
+    {
+      $merge: 'reviews_metadata'
+    }
+  ]);
+
+  await cursor.toArray();
+  // end timer and print the duration
+  console.timeEnd('create reviews_metadata');
+
+  // close db connection
+  client.close();
+};
+
+module.exports = createReviewsMetadata;
---
+++ database/ETL/createReviewsPhotosCollection.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+
+const createReviewsPhotosCollection = async () => {
+  const client = await MongoClient.connect('mongodb://localhost:27017', {
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db('hr-sdc-manticore');
+
+  // define collections
+  const reviewsPhotos = db.collection('reviews_photos');
+
+  // create indexes
+  await reviewsPhotos.createIndex({ id: 1 });
+
+  // start timer
+  console.time('create reviews_photos_collection');
+
+  const cursor = reviewsPhotos.aggregate([
+    {
+      $group: {
+        _id: '$review_id',
+        photos: {
+          $push: { id: '$id', url: '$url' }
+        }
+      }
+    },
+    {
+      $addFields: { review_id: '$_id' }
+    },
+    {
+      $merge: 'reviews_photos_collection'
+    }
+  ]);
+
+  await cursor.toArray();
+  // end timer and print the duration
+  console.timeEnd('create reviews_photos_collection');
+
+  // close db connection
+  client.close();
+};
+
+module.exports = createReviewsPhotosCollection;
