8ed1a090b9c086527964a8c146f7b34da4ec8068
Yichen <chenlin7789@gmail.com>
2023-07-19 15:38:09 -0700

    changed default value from 'relevant' to 'newest' for now because I don't have a relevant sort method yet

--- server/controllers/productReview.js
+++ server/controllers/productReview.js
@@ -4,7 +4,7 @@ exports.getReviews = async (req, res) => {
   let reviews = null;
   const queryParams = Object.keys(req.query);
   const product_id = Number(req.query.product_id);
-  const sort_by_field = req.query.sort || 'relevant';
+  const sort_by_field = req.query.sort || 'newest'; // TODO: change it to 'relevant'
   const page = Number(req.query.page) || 1;
   const count = Number(req.query.count) || 5;
   try {
